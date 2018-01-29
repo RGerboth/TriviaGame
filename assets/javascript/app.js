71212484c15b8100984216ee817f4aee4406ccd9
Robert Gerboth <robert.gerboth@gmail.com>
2018-01-29 16:21:29 -0700

    Added some style elements, five questions total

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,9 +10,29 @@
 	var correctSound = new Audio("http://www.wavsource.com/snds_2018-01-14_3453803176249356/movies/monty_python_hg/so_be_it.wav");
 	var mouseClick = new Audio("http://www.kalmanovitz.co.il/courses/English/construction/Assets/Mousclik.wav");
 
-	var questions=[["Stop! WHAT is your name?"],["What is your favorite color?"],["What is your quest?"]];
-	var answers=[["Sir Gawain","It is Arthur, King of the Britons!","Sir Lancelot", "Fred"],["Red","Green","Blue","Orange"],["To find Camelot","To obtain a shrubbery","To save Guinevere","We seek the Holy Grail"]];
-	var correctAnswers=[["B. It is Arthur, King of the Britons!"],["C. Blue"],["D. We seek the Holy Grail"]];
+	var questions=[
+	["Stop! WHAT is your name?"],
+	["What is your favorite color?"],
+	["What is your quest?"],
+	["What is the captol of Assyria?"],
+	["What is the airspeed velocity of a unlaiden swallow?"]
+	];
+
+	var answers=[
+	["Sir Gawain","It is Arthur, King of the Britons!","Sir Lancelot", "Fred"],
+	["Red","Green","Blue","Orange"],
+	["To find Camelot","To obtain a shrubbery","To save Guinevere","To seek the Holy Grail"],
+	["Assur","Neneveh","Babylon","Sumer"],
+	["African, or European?", "25 miles per hour", "I don't know that!", "35 miles per hour"]
+	];
+	
+	var correctAnswers=[
+	["B. It is Arthur, King of the Britons!"],
+	["C. Blue"],
+	["D. To seek the Holy Grail"],
+	["A. Assur"],
+	["A. African, or European?"]
+	];
 
 	function startScreen() {
 		currentQuestion=0;
@@ -21,18 +41,18 @@
 	}
 
 	function displayCorrectAnswer() {
-		answerScreen = "<p class='answerDisplay'>You are correct! <br>" + correctAnswers[currentQuestion] + "</p>";
+		answerScreen = "<p class='answerDisplay'>You are correct! <br>" + correctAnswers[currentQuestion] + "</p><img src='assets/images/SoBeIt.gif' alt='So be it!'</img>";
 		console.log(answerScreen);
 		$("#body").html(answerScreen);
 	}
 
 	function displayIncorrectAnswer() {
-		answerScreen = "<p class='answerDisplay'>Incorrect! The correct answer is: <br>" + correctAnswers[currentQuestion] + "</p>";
+		answerScreen = "<p class='answerDisplay'>Incorrect! The correct answer is: <br>" + correctAnswers[currentQuestion] + "</p><img src='assets/images/RunAway.gif' alt='Run away!'</img>";
 		console.log(answerScreen);
 		$("#body").html(answerScreen);
 	}
 	function displayTimeoutAnswer() {
-		answerScreen = "<p class='answerDisplay'>Time Out! The correct answer is: <br>" + correctAnswers[currentQuestion] + "</p>";
+		answerScreen = "<p class='answerDisplay'>Time Out! The correct answer is: <br>" + correctAnswers[currentQuestion] + "</p><img src='assets/images/wrongAnswer.gif' alt='dreaded black beast'</img>";
 		console.log(answerScreen);
 		$("#body").html(answerScreen);
 	}
@@ -47,7 +67,7 @@
 
   	//display a quesion
   	function displayQuestion() {
-		displayHTML = "<p class='text-center'>" + questions[currentQuestion] + "</p><p class='first-answer answer'>A. " + answers[currentQuestion][0] + "</p><p class='answer'>B. " + answers[currentQuestion][1] + "</p><p class='answer'>C. " + answers[currentQuestion][2] + "</p><p class='answer'>D. " + answers[currentQuestion][3] + "</p>";
+		displayHTML = "<p class='text-center question'>" + questions[currentQuestion] + "</p><button class='first-answer answer'>A. " + answers[currentQuestion][0] + "</button><button class='answer'>B. " + answers[currentQuestion][1] + "</button><button class='answer'>C. " + answers[currentQuestion][2] + "</button><button class='answer'>D. " + answers[currentQuestion][3] + "</button>";
 		$("#body").html(displayHTML);
 		startAnswerCountdown();
 		console.log("Current Question: " + currentQuestion)
@@ -56,7 +76,7 @@
 	function resultsScreen() {
 		console.log("Display Results Screen?")
 		currentQuestion=0;
-		results = "<p class='results'>Correct Guesses: " + correctGuesses + "</p><p class='results'>Incorrect Guesses: " + incorrectGuesses + "</p><p class='results'>Unanswered: " + unanswered + "</p><p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Play Again</a></p>";
+		results = "<p class='results'>Correct Answers: " + correctGuesses + "</p><p class='results'>Incorrect Answers: " + incorrectGuesses + "</p><p class='results'>Unanswered: " + unanswered + "</p><p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Play Again</a></p>";
 		console.log(results);
 		$("#body").html(results);
 	}
@@ -107,7 +127,7 @@
 	    }, 1000);
 	}
 
-	//when click on start-button, also works on reset
+	//when click on start and reset
 	$("body").on("click", ".start-button", function(event){
 		launchSound.play();
 		correctGuesses=0;
@@ -128,4 +148,6 @@
 		else {
 			incorrectAnswer();
 		}
+
+
 	}); // end answer click
